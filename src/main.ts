2040f84d4e52302cbdf1dbbbbe8ff1a035b1dcdc
Doniyorov Doniyor <ddonierov68@gmail.com>
2023-05-23 11:32:25 +0500

    deploy

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
   app.enableCors({
-    origin: 'http://localhost:3000'
+    origin: 'https://next-crud-system.onrender.com'
   })
   await app.listen(8080);
 }
